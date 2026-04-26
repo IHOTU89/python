3b582bbec76571b694b3f814cc2f9aaec9a7f95b
IHOTU89 <amicable269@gmail.com>
2026-04-26 12:26:05 +0100

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,11 @@
+class rectangle():
+   def___init___(self, l ,w)
+self .length=l
+self.width=w
+
+def rectangle_area (self):
+    return self.length*self.width
+  
+ newrectangle=Rectangle(12,10)
+ print("dimension of rectangle-length:%d width:%d" %(newrectangle.length newrectangle.width))
+ print("area of rectangle:",newrectangle.rectangle_area())
